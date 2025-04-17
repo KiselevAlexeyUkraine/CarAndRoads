c1dc18229f71305a16f8c5f274508bcd7ef9efde
KiselevAlexey <alekskisliy@gmail.com>
2025-04-17 11:55:24 +0300

    Контроллер для игрока

---
+++ Assets/CodeBase/Components/Player/PlayerController.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using Codebase.Services.Inputs;
+using UnityEngine;
+using Zenject;
+
+namespace Codebase.Components.Player
+{
+    [RequireComponent(typeof(CharacterController))]
+    public class PlayerController : MonoBehaviour
+    {
+        public event Action OnMoving;
+
+        private float _maxXPosition = 6f;
+        private float _minXPosition = -6f;
+
+        private CharacterController _characterController;
+        private float _currentXPosition;
+        private DesktopInput _input = new();
+
+
+        //[Inject]
+        //private void Construct(IInputService inputService, PauseService pauseService)
+        //{
+        //    _input = inputService;
+        //    _pause = pauseService;
+        //}
+
+        private void Awake()
+        {
+            _characterController = GetComponent<CharacterController>();
+        }
+ 
+        private void Update()
+        {
+            HandleMovement();
+        }  
+
+        private void HandleMovement()
+        {
+            if (_input.Left)
+            {
+                MoveLeft();
+            }
+
+            if (_input.Right)
+            {
+                MoveRight();
+            }
+        }
+
+        private void MoveLeft()
+        {
+            if (_currentXPosition > _minXPosition)
+            {
+                _currentXPosition -= 3f;
+                TeleportPlayer();
+                OnMoving?.Invoke();
+            }
+        }
+
+        private void MoveRight()
+        {
+            if (_currentXPosition < _maxXPosition)
+            {
+                _currentXPosition += 3f;
+                TeleportPlayer();
+                OnMoving?.Invoke();
+            }
+        }
+
+        private void TeleportPlayer()
+        {
+            var targetPosition = new Vector3(_currentXPosition, transform.position.y, transform.position.z);
+            var moveDirection = targetPosition - transform.position;
+
+            _characterController.Move(moveDirection);
+        }
+    }
+}
